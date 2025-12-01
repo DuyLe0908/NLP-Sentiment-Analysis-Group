b54efe523ec4d5f5cc794b7436cff8e2881e97ee
legia <legianhatduy2003@gmail.com>
2025-12-01 19:21:29 +1030

    Add required imports to data_preparation.py

---
+++ src/data_preparation.py
@@ -0,0 +1,7 @@
+import os
+import re
+import random
+import pickle
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
